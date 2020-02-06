474ce3b8643f5f816f7227d0f0ddd5e287831740
miaoxiaolan <2556253092@qq.com>
2020-02-06 23:55:35 +0800

    转盘抽奖

--- rotatewheel/server.php
+++ rotatewheel/server.php
@@ -4,3 +4,19 @@ header('content-type:application/json');
 header('Access-Control-Allow-Origin:*');
 //允许访问的方式
 // header('Access-Control-Allow-Method:POST,GET');
+require_once "DBHelper.php";
+$con=new  DBHelper();
+
+
+
+$res=0;
+$jf=$_POST['integral'];
+$userid=$_POST['userid'];
+if( 60<=$jf){
+    $jf=$jf-60;
+    //修改积分数据
+    $result = $con->exec('integral',["jf"=>$jf],"update","userid=".$userid);
+    rand(0,6);
+    echo json_encode($result);
+
+}
