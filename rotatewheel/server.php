441f2e43db1a3ac9f93401ccab2e10209838c201
miaoxiaolan <2556253092@qq.com>
2020-02-07 21:38:52 +0800

    完善了积分和后端概率控制

--- rotatewheel/server.php
+++ rotatewheel/server.php
@@ -4,19 +4,29 @@ header('content-type:application/json');
 header('Access-Control-Allow-Origin:*');
 //允许访问的方式
 // header('Access-Control-Allow-Method:POST,GET');
-require_once "DBHelper.php";
-$con=new  DBHelper();
-
-
-
+// require_once "../../DBHelper.php";
+// $con=new  DBHelper();
 $res=0;
 $jf=$_POST['integral'];
 $userid=$_POST['userid'];
+$num;
 if( 60<=$jf){
     $jf=$jf-60;
     //修改积分数据
-    $result = $con->exec('integral',["jf"=>$jf],"update","userid=".$userid);
-    rand(0,6);
-    echo json_encode($result);
+    // $result = $con->exec('integral',["jf"=>$jf],"update","userid=".$userid);
+    $rd=rand(0,100);  
 
+    if($rd<=65){
+        $num=0;
+    }elseif($rd <= 95){
+        $num=rand(1,6);
+    }else{
+        $num=7;
+    }
+   
+
+}else{
+    $num=8;
 }
+// echo $num;
+echo json_encode($num);
